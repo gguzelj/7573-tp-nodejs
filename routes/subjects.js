d190702eec62c3c1242d0d5d55d11f5abcff4829
lmasello <maselloleandro@gmail.com>
2016-10-17 00:05:58 -0300

    Initial views structure.

--- routes/subjects.js
+++ routes/subjects.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.json({ message: 'GET all subjects endpoint!' });
+    res.redirect('views/landing/index.html')
 });
 
 router.get('/:subject_id', function(req, res) {
